2fc7f62369eafa7e698b312a55f5b7e2dc5c55a8
Ivan Milinkovic <ivanmcode@gmail.com>
2025-09-27 20:15:08 +0200

    Reorganize apple project directories, add UI app

---
+++ apple/RaytracerWeekendApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RaytracerWeekendApp
+//
+//  Created by Ivan Milinkovic on 27. 9. 2025..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apple/RaytracerWeekendApp/RaytracerWeekendAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaytracerWeekendAppApp.swift
+//  RaytracerWeekendApp
+//
+//  Created by Ivan Milinkovic on 27. 9. 2025..
+//
+
+import SwiftUI
+
+@main
+struct RaytracerWeekendAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
