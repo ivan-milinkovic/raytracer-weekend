a983297504a5676f05739ad0a517e55ab5b33231
Ivan Milinkovic <ivanmcode@gmail.com>
2025-10-05 23:11:45 +0200

    One buffer to copy the image to while rendering progress

--- apple/RaytracerWeekendApp/RenderController.swift
+++ apple/RaytracerWeekendApp/RenderController.swift
@@ -21,42 +21,72 @@ func renderProgressCallback(progress: Double) {
     }
 }
 
+func setup_callbacks() {
+    rw_set_render_pass_callback(callbackFromRender)
+    rw_set_render_progress_callback(renderProgressCallback)
+}
 
-class RenderController: ObservableObject {
+final class RenderController: ObservableObject {
     
-    let renderQueue = DispatchQueue.init(label: "render-queue", qos: .userInitiated)
-    
-    @Published var cgImage: CGImage?
-    @Published var progress: Double = 0
-    @Published var isRendering: Bool = false
     @Published var sceneId: Int = 1
+    @Published private(set) var cgImage: CGImage?
+    @Published private(set) var isRendering: Bool = false
+    @Published fileprivate(set) var progress: Double = 0
+    
+    private let renderQueue = DispatchQueue.init(label: "render-queue", qos: .userInitiated)
+    var copyPxPtr: UnsafeMutableRawPointer?
     
     func setup() {
-        rw_set_render_pass_callback(callbackFromRender)
-        rw_set_render_progress_callback(renderProgressCallback)
+        setup_callbacks()
     }
     
     func render() {
+        if isRendering { return }
         isRendering = true
+        progress = 0.0
         cgImage = nil
         let sceneId: Int32 = Int32(self.sceneId)
+        copyPxPtr?.deallocate()
+        copyPxPtr = nil
+        
         renderQueue.async { [sceneId] in
             rwmain(sceneId)
+            self.handle(imgPtr: rw_get_raw_image(), isFinal: true)
             DispatchQueue.main.async {
                 self.isRendering = false
             }
         }
     }
     
-    func handle(imgPtr: UnsafeMutablePointer<RawImage>) {
-        let img = rw_get_raw_image().pointee
+    func handle(imgPtr: UnsafeMutablePointer<RawImage>, isFinal: Bool = false) {
+        let rawImg = rw_get_raw_image().pointee
+        let bytes = rawImg.bytes!
+        let w = Int(rawImg.w)
+        let h = Int(rawImg.h)
+        let pixelSize = Int(rawImg.pixel_size)
+        let byteCount = w * h * pixelSize
         
-        let w = Int(img.w)
-        let h = Int(img.h)
-        let pixelSize = Int(img.pixel_size)
+        // avoids copying large buffers on each update
+        // creates one at the beginning and destroys it at the end of render
+        if copyPxPtr == nil {
+            copyPxPtr = UnsafeMutableRawPointer.allocate(byteCount: byteCount, alignment: 1)
+        }
+        
+        let imgBytesRawPtr = UnsafeMutableRawPointer(mutating: bytes)
+        copyPxPtr!.copyMemory(from: imgBytesRawPtr, byteCount: byteCount)
         
         autoreleasepool {
-            let cgImg = Images.cgImageSRGB(img.bytes, w: w, h: h, pixelSize: pixelSize)
+            // let cgImg = Images.cgImageSRGB(bytes, w: w, h: h, pixelSize: pixelSize)
+            let data: Data
+            if isFinal {
+                data = Data(bytes: copyPxPtr!, count: w * h * pixelSize)
+                copyPxPtr?.deallocate()
+                copyPxPtr = nil
+            } else {
+                data = Data.init(bytesNoCopy: copyPxPtr!, count: w * h * pixelSize, deallocator: .none)
+            }
+            let cgImg = Images.cgImageSRGB(pixelData: data, w: w, h: h, pixelSize: pixelSize)
+            
             DispatchQueue.main.async {
                 self.cgImage = cgImg
             }
