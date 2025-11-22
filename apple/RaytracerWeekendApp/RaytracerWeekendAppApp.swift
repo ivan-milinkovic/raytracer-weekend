f91d69c40476733090e95f7aaa0ab77524ab037a
Ivan Milinkovic <ivanmcode@gmail.com>
2025-11-23 00:04:40 +0100

    Quit on window close

--- apple/RaytracerWeekendApp/RaytracerWeekendAppApp.swift
+++ apple/RaytracerWeekendApp/RaytracerWeekendAppApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct RaytracerWeekendAppApp: App {
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
     }
 }
